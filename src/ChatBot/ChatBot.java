76ca9814326c75116a6e818426114f54ba60ba4b
“Sasha_Kovzhenko” <o.i.kovzhenko@student.khai.edu>
2021-10-13 10:00:27 +0300

    ChatBot 2-st stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,2 +1,14 @@
-package ChatBot;public class ChatBot {
+package ChatBot;
+
+import java.util.Scanner;
+
+public class ChatBot {
+    public static void main(String []args) {
+        System.out.println("Hello! My name is Jora.\nI was created in 2021.\nPlease, remind me your name.");
+        Scanner scanner = new Scanner(System.in);
+        String response;
+        response = scanner.nextLine();
+        System.out.println("What a great name you have, "+response);
+    }
+
 }
