649cf6a4e505cd867d96cfa0e98d69eda0f818df
“Sasha_Kovzhenko” <o.i.kovzhenko@student.khai.edu>
2021-10-23 14:47:39 +0300

    Hangman - 1 Stage

---
+++ src/Hangman/hangman.java
@@ -0,0 +1,10 @@
+package Hangman;
+
+public class hangman {
+    public hangman(){
+    }
+    public static void main(String[] args){
+        System.out.println("HANGMAN\nThe game will be available soon.");
+
+    }
+}
