6c0596c7024452acd92818a302cc897828534868
“Sasha_Kovzhenko” <o.i.kovzhenko@student.khai.edu>
2021-11-10 17:05:06 +0200

    Hangman - 2 stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -1,10 +1,19 @@
 package Hangman;
+import java.util.Scanner;
 
 public class hangman {
-    public hangman(){
-    }
     public static void main(String[] args){
         System.out.println("HANGMAN\nThe game will be available soon.");
+        String a = "python", b;
+        Scanner in = new Scanner(System.in);
+        b = in.nextLine();
+        if (a.equals(b)){
+            System.out.println("You survived!");
+        }
+        else {
+            System.out.println("You Lost!");
+        }
 
     }
+
 }
