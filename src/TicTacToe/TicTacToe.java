bff77b8087160190a2522294c637c7c751118a0d
“Sasha_Kovzhenko” <o.i.kovzhenko@student.khai.edu>
2021-12-08 23:56:54 +0200

    TicTacToe - 3 stage

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -14,9 +14,49 @@ public class TicTacToe {
             for (int i1 = 0; i1 < 3; i1 ++){
                 System.out.print(field.charAt(num));
                 num ++;
-            }
-            System.out.println("|");
+            }System.out.println("|");
+        }System.out.println("-----");
+        if (((field.charAt(0) == 'X' && field.charAt(1) == 'X' && field.charAt(2) == 'X') ||
+                (field.charAt(3) == 'X' && field.charAt(4) == 'X' && field.charAt(5) == 'X') ||
+                (field.charAt(6) == 'X' && field.charAt(7) == 'X' && field.charAt(8) == 'X') ||
+                (field.charAt(0) == 'X' && field.charAt(3) == 'X' && field.charAt(6) == 'X') ||
+                (field.charAt(1) == 'X' && field.charAt(4) == 'X' && field.charAt(7) == 'X') ||
+                (field.charAt(2) == 'X' && field.charAt(5) == 'X' && field.charAt(8) == 'X') ||
+                (field.charAt(0) == 'X' && field.charAt(4) == 'X' && field.charAt(8) == 'X') ||
+                (field.charAt(2) == 'X' && field.charAt(4) == 'X' && field.charAt(6) == 'X'))
+        && ((field.charAt(0) == 'O' && field.charAt(1) == 'O' && field.charAt(2) == 'O') ||
+                (field.charAt(3) == 'O' && field.charAt(4) == 'O' && field.charAt(5) == 'O') ||
+                (field.charAt(6) == 'O' && field.charAt(7) == 'O' && field.charAt(8) == 'O') ||
+                (field.charAt(0) == 'O' && field.charAt(3) == 'O' && field.charAt(6) == 'O') ||
+                (field.charAt(1) == 'O' && field.charAt(4) == 'O' && field.charAt(7) == 'O') ||
+                (field.charAt(2) == 'O' && field.charAt(5) == 'O' && field.charAt(8) == 'O') ||
+                (field.charAt(0) == 'O' && field.charAt(4) == 'O' && field.charAt(8) == 'O') ||
+                (field.charAt(2) == 'O' && field.charAt(4) == 'O' && field.charAt(6) == 'O'))){
+            System.out.println("Impossible");
+        }else if ((field.charAt(0) == 'X' && field.charAt(1) == 'X' && field.charAt(2) == 'X') ||
+                (field.charAt(3) == 'X' && field.charAt(4) == 'X' && field.charAt(5) == 'X') ||
+                (field.charAt(6) == 'X' && field.charAt(7) == 'X' && field.charAt(8) == 'X') ||
+                (field.charAt(0) == 'X' && field.charAt(3) == 'X' && field.charAt(6) == 'X') ||
+                (field.charAt(1) == 'X' && field.charAt(4) == 'X' && field.charAt(7) == 'X') ||
+                (field.charAt(2) == 'X' && field.charAt(5) == 'X' && field.charAt(8) == 'X') ||
+                (field.charAt(0) == 'X' && field.charAt(4) == 'X' && field.charAt(8) == 'X') ||
+                (field.charAt(2) == 'X' && field.charAt(4) == 'X' && field.charAt(6) == 'X')){
+            System.out.println("X win");
+        }else if ((field.charAt(0) == 'O' && field.charAt(1) == 'O' && field.charAt(2) == 'O') ||
+                (field.charAt(3) == 'O' && field.charAt(4) == 'O' && field.charAt(5) == 'O') ||
+                (field.charAt(6) == 'O' && field.charAt(7) == 'O' && field.charAt(8) == 'O') ||
+                (field.charAt(0) == 'O' && field.charAt(3) == 'O' && field.charAt(6) == 'O') ||
+                (field.charAt(1) == 'O' && field.charAt(4) == 'O' && field.charAt(7) == 'O') ||
+                (field.charAt(2) == 'O' && field.charAt(5) == 'O' && field.charAt(8) == 'O') ||
+                (field.charAt(0) == 'O' && field.charAt(4) == 'O' && field.charAt(8) == 'O') ||
+                (field.charAt(2) == 'O' && field.charAt(4) == 'O' && field.charAt(6) == 'O')) {
+            System.out.println("O win");
+        }else if (field.charAt(0) == '_' || field.charAt(1) == '_' || field.charAt(2) == '_' ||
+                field.charAt(3) == '_' || field.charAt(4) == '_' || field.charAt(5) == '_' ||
+                field.charAt(6) == '_' || field.charAt(7) == '_' || field.charAt(8) == '_'){
+            System.out.println("Game not finished");
+        }else{
+            System.out.println("Draw");
         }
-        System.out.println("-----");
     }
 }
