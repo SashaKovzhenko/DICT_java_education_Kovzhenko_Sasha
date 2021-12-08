5d35f112bce879c642c4a8ea88a1ed22df336612
“Sasha_Kovzhenko” <o.i.kovzhenko@student.khai.edu>
2021-12-09 00:40:43 +0200

    TicTacToe - 5 stage

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -5,13 +5,13 @@ public class TicTacToe {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         int num = 0;
+        int turn = 0;
         int correct = 0, correct2 = 0;
         boolean end = false;
         int[] coordinates = new int[2];
         String field;
         char[] field2 = new char[9];
-        System.out.print("Enter cells: ");
-        field = in.next();
+        field = "_________";
         for (int i5 = 0; i5 < 9; i5 ++){
             field2[i5] = field.charAt(i5);
         }do{
@@ -42,8 +42,41 @@ public class TicTacToe {
                 if (field2[correct] == 'X' || field2[correct] == 'O'){
                     System.out.println("This cell is occupied! Choose another one!");
                 }else{
-                    field2[correct] = 'X';
+                    if (turn == 0){
+                        field2[correct] = 'X';
+                        turn ++;
+                    }else if (turn == 1){
+                        field2[correct] = 'O';
+                        turn --;
+                    }
                 }
+            }if ((field2[0] == 'X' && field2[1] == 'X' && field2[2] == 'X') ||
+                    (field2[3] == 'X' && field2[4] == 'X' && field2[5] == 'X') ||
+                    (field2[6] == 'X' && field2[7] == 'X' && field2[8] == 'X') ||
+                    (field2[0] == 'X' && field2[3] == 'X' && field2[6] == 'X') ||
+                    (field2[1] == 'X' && field2[4] == 'X' && field2[7] == 'X') ||
+                    (field2[2] == 'X' && field2[5] == 'X' && field2[8] == 'X') ||
+                    (field2[0] == 'X' && field2[4] == 'X' && field2[8] == 'X') ||
+                    (field2[2] == 'X' && field2[4] == 'X' && field2[6] == 'X')){
+                System.out.println("X win");
+                System.exit(0);
+            } else if ((field2[0] == 'O'&& field2[1] == 'O' && field2[2] == 'O')||
+                    (field2[3] == 'O' && field2[4] == 'O' && field2[5] == 'O') ||
+                    (field2[6] == 'O' && field2[7] == 'O' && field2[8] == 'O') ||
+                    (field2[0] == 'O' && field2[3] == 'O' && field2[6] == 'O') ||
+                    (field2[1] == 'O' && field2[4] == 'O' && field2[7] == 'O') ||
+                    (field2[2] == 'O' && field2[5] == 'O' && field2[8] == 'O') ||
+                    (field2[0] == 'O' && field2[4] == 'O' && field2[8] == 'O') ||
+                    (field2[2] == 'O' && field2[4] == 'O' && field2[6] == 'O')){
+                System.out.println("O win");
+                System.exit(0);
+            }else if (field2[0] == '_' || field2[1] == '_' || field2[2] == '_' ||
+                    field2[3] == '_' || field2[4] == '_' || field2[5] == '_' ||
+                    field2[6] == '_' || field2[7] == '_' || field2[8] == '_'){
+                System.out.println("Game not finished");
+            }else {
+                System.out.println("Draw");
+                System.exit(0);
             }
         }while (true);
     }
