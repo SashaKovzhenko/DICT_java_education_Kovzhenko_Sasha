231fc04bdc7c345d293f462e767c085da24c9831
“Sasha_Kovzhenko” <o.i.kovzhenko@student.khai.edu>
2021-12-09 12:55:26 +0200

    CreditCalculator - 4 stage

--- src/CreditCalculator/CreditCalculator.java
+++ src/CreditCalculator/CreditCalculator.java
@@ -1,60 +1,77 @@
 package CreditCalculator;
 
-import java.util.Scanner;
-
 public class CreditCalculator {
     public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        double sum_of_credit, payment, n, i, i_nominal;
+        double sum_of_credit, payment, n, i, i_nominal, D, overpay = 0;
         int months, years;
         char want;
-        System.out.println("What do you want to calculate?");
-        System.out.println("type \"n\" – for number of monthly payments,");
-        System.out.println("type \"a\" – for annuity monthly payment amount,");
-        System.out.println("type \"p\" – for sum of credit");
-        want = in.next().charAt(0);
-
-        if (want == 'n'){
-            System.out.println("Enter the sum of credit: ");
-            sum_of_credit = in.nextDouble();
-            System.out.print("Enter the monthly payment: ");
-            payment = in.nextDouble();
-            System.out.print("Enter the loan interest: ");
-            i = in.nextInt();
-            i_nominal = i / (12 * 100);
-            n = Math.log(payment / (payment - (i_nominal * sum_of_credit))) / Math.log(1 + i_nominal);
-            n = Math.ceil(n);
-            months = (int) n;
-            if (n % 12 > 0){
-                years = months / 12;
-                months = months - years * 12;
-                System.out.println("It will take " + years + " years and " + months + " months to repay this credit!");
+        String want1 = System.getProperty("want");
+        String sum_of_credit1 = System.getProperty("sum_of_credit");
+        String payment1 = System.getProperty("payment");
+        String i1 = System.getProperty("i");
+        String months1 = System.getProperty("months");
+        if (want1 != null){
+            want = want1.charAt(0);
+            if (want == 'a'){
+                if (sum_of_credit1 != null && payment1 != null && i1 != null) {
+                    sum_of_credit = Double.parseDouble(sum_of_credit1);
+                    payment = Double.parseDouble(payment1);
+                    i = Integer.parseInt(i1);
+                    i_nominal = i / (12 * 100);
+                    n = Math.log(payment / (payment - (i_nominal * sum_of_credit))) / Math.log(1 + i_nominal);
+                    n = Math.ceil(n);
+                    months = (int) n;
+                    if (n % 12 > 0) {
+                        years = months / 12;
+                        months = months - years * 12;
+                        System.out.println("It will take " + years + " years and " + months + " months to repay this credit!");
+                    } else {
+                        years = months / 12;
+                        System.out.println("It will take " + years + " years to repay this credit!");
+                    }
+                } else if (sum_of_credit1 != null && months1 != null && i1 != null) {
+                    months = Integer.parseInt(months1);
+                    sum_of_credit = Double.parseDouble(sum_of_credit1);
+                    i = Integer.parseInt(i1);
+                    i_nominal = i / (12 * 100);
+                    payment = sum_of_credit * ((i_nominal * Math.pow((1 + i_nominal), months)) / (Math.pow((1 + i_nominal), months) - 1));
+                    payment = Math.ceil(payment);
+                    System.out.println("Your monthly payment = " + (int)payment + "!");
+                } else if (months1 != null && payment1 != null && i1 != null) {
+                    months = Integer.parseInt(months1);
+                    i = Integer.parseInt(i1);
+                    payment = Double.parseDouble(payment1);
+                    i_nominal = i / (12 * 100);
+                    sum_of_credit = payment / ((i_nominal * Math.pow((1 + i_nominal), months)) / (Math.pow((1 + i_nominal), months) - 1));
+                    sum_of_credit = Math.ceil(sum_of_credit);
+                    System.out.println("Your sum of credit = " + (int)sum_of_credit + "!");
+                } else {
+                    System.out.println("Incorrect parameters.");
+                    System.exit(0);
+                }
+            }else if (want == 'd'){
+                if (sum_of_credit1 == null || months1 == null || i1 == null) {
+                    System.out.println("Incorrect parameters.");
+                    System.exit(0);
+                }
+                months = Integer.parseInt(months1);
+                sum_of_credit = Double.parseDouble(sum_of_credit1);
+                i = Integer.parseInt(i1);
+                i_nominal = i / (12 * 100);
+                for (int m = 1; m <= months; m ++) {
+                    D = sum_of_credit / months + i_nominal * (sum_of_credit - ((sum_of_credit * (m - 1)) / (months)));
+                    D = Math.ceil(D);
+                    overpay = overpay + (D - (sum_of_credit / months));
+                    System.out.println("Month " + m + ": payment is " + (int)D);
+                }
+                System.out.println("Overpayment = " + (int)overpay);
             }else{
-                years = months / 12;
-                System.out.println("It will take " + years + " years to repay this credit!");
+                System.out.println("Incorrect parameters.");
+                System.exit(0);
             }
-        }else if (want == 'a'){
-            System.out.print("Enter the sum of credit: ");
-            sum_of_credit = in.nextDouble();
-            System.out.print("Enter the number of periods: ");
-            months = in.nextInt();
-            System.out.print("Enter the loan interest: ");
-            i = in.nextInt();
-            i_nominal = i / (12 * 100);
-            payment = sum_of_credit * ((i_nominal * Math.pow((1 + i_nominal), months)) / (Math.pow((1 + i_nominal), months)-1));
-            payment = Math.ceil(payment);
-            System.out.println("Your monthly payment = " + (int)payment + "!");
-        }else if (want == 'p'){
-            System.out.print("Enter the payment: ");
-            payment = in.nextDouble();
-            System.out.print("Enter the number of periods: ");
-            months = in.nextInt();
-            System.out.print("Enter the loan interest: ");
-            i = in.nextDouble();
-            i_nominal = i / (12 * 100);
-            sum_of_credit = payment / ((i_nominal * Math.pow((1 + i_nominal), months)) / (Math.pow((1 + i_nominal), months)-1));
-            sum_of_credit = Math.ceil(sum_of_credit);
-            System.out.println("Your sum of credit = " + (int)sum_of_credit + "!");
+        }else {
+            System.out.println("Incorrect parameters.");
+            System.exit(0);
         }
     }
 }
